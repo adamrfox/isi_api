85f58c06e6abb1da7fed37dff6641efdabd8b3f6
adamfox <adamrfox@yahoo.com>
2021-12-06 11:32:43 -0500

    Implemented DELETE and POST

--- isi_api.py
+++ isi_api.py
@@ -21,17 +21,25 @@ def python_input(message):
         val = raw_input(message)
     return (val)
 
+def dprint(message):
+    if DEBUG:
+        print(message)
+
 if __name__ == "__main__":
     PORT = 8080
     user = ""
     password = ""
     HEADERS = {'Content-Type': 'application/json'}
     OP = "GET"
+    services = ['platform']
+    DEBUG = False
 
-    optlist, args = getopt(sys.argv[1:], 'hc:o:', ['--help', '--creds=', '--op='])
+    optlist, args = getopt(sys.argv[1:], 'hDc:o:s', ['--help', '--DEBUG', '--creds=', '--op=', '--services'])
     for opt, a in optlist:
         if opt in ('-h', '--help'):
             usage()
+        if opt in ('-D', '--DEBUG'):
+            DEBUG = True
         if opt in ('-c', '--creds'):
             (user, password) = a.split(':')
         if opt in ('-o', '--op'):
@@ -40,6 +48,9 @@ if __name__ == "__main__":
             else:
                 sys.stderr.write("Accepted ops are: GET, POST, DELETE\n")
                 exit(1)
+        if opt in ('-s', '--services'):
+            services = a.split(',')
+
     if not user:
         user = python_input("User: ")
     if not password:
@@ -48,15 +59,25 @@ if __name__ == "__main__":
         (host, endpoint) = args
     else:
         (host, data, endpoint) = args
+        data_j = json.loads(data)
     uri = "https://" + host + ":" + str(PORT)
     session = requests.Session()
     data = json.dumps({'username': user, 'password': password, 'services': ['platform']})
     resp = session.post(uri + '/session/1/session/', data=data, headers=HEADERS, verify=False)
     session.headers['referer'] = uri
     session.headers['X-CSRF-Token'] = session.cookies.get('isicsrf')
-    print(session.headers)
+    dprint("HEADERS: " + str(session.headers))
     if OP == "GET":
         resp = session.get(uri + endpoint, verify=False)
         pprint(json.loads(resp.content))
+    elif OP == "DELETE":
+        resp = session.delete(uri + endpoint, verify=False)
+        if resp.status_code != 204:
+            pprint(json.loads(resp.content))
+    elif OP == "POST":
+        resp = session.post(uri + endpoint, json=data_j, verify=False)
+        if resp.status_code != 201:
+            pprint(json.loads(resp.content))
+
 
 
